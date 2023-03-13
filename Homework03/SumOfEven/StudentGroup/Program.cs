431aab4804f865196edb10a4670a913e97216830
nj9x <njovikj@gmail.com>
2023-03-13 19:43:32 +0100

    Add files via upload

---
+++ Homework03/SumOfEven/StudentGroup/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Student Group");
+string[] studentsG1 = new string[] { "Nenad", "Marko", "Sanja", "Maja", "Petar" };
+string[] studentsG2 = new string[] { "Kristina", "Ana", "Vasko", "Nikola", "Aleksandar" };
+
+Console.WriteLine("Enter an appropriate group number, either 1 or 2");
+string input = Console.ReadLine();
+
+int groupNumber;
+
+if (!int.TryParse(input, out groupNumber) || (groupNumber != 1 && groupNumber != 2))
+{
+    Console.WriteLine("Invalid Input, enter 1 or 2");
+    return;
+}
+
+Console.WriteLine($"The selected students in {groupNumber} are: ");
+if (groupNumber == 1)
+{
+    foreach (string student in studentsG1)
+    {
+        Console.WriteLine(student);
+    }
+}
+else if (groupNumber == 2)
+{
+    foreach (string student in studentsG2)
+    {
+        Console.WriteLine(student);
+    }
+}
---
+++ Homework03/SumOfEven/SumOfEven/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+Console.WriteLine("SumOfEven Application");
+
+int[] intArray = new int[] { 2, 4, 5, 7, 8, 10, 12, 33, 43, 120 };
+int sum  = 0;
+
+for (int i = 0; i < intArray.Length; i++)
+{
+    if (intArray[i] % 2 == 0)
+    {
+        sum += intArray[i];
+    }
+}
+Console.WriteLine(sum);
